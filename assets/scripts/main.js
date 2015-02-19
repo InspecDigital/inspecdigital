49304fbffe6d7b2d9e328bd5e1a929e6943966ca
Rick Vodicka <rick@inspecdigital.org>
2015-02-18 16:02:26 -0800

    Style changes, content updates, navbar hover effects and placeholder screenshots

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,49 @@
  * @fileOverview : main.js
  */
 
+ var config = {
+  navbar : {
+    link : $('#nav-primary').find('a'),
+    icon : 'i'
+  }
+ };
+
 function AppCore() {
 
-	return this;
+    'use strict';
+
+    this._construct = function(self) {
+
+      self.navbar();
+
+      return self;
+
+    }
+
+    this.navbar = function(options) {
+
+      var _options = {
+        link : '',
+        icon : ''
+      };
+
+      $.extend(true, _options, config.navbar, options);
+
+      $(_options.link).on({
+        mouseenter : function() {
+          $(this).find(_options.icon).addClass('visible');
+        },
+        mouseleave : function() {
+          $(this).find(_options.icon).removeClass('visible');
+        }
+      });
+
+      return this;
+
+    }
+
+  return this._construct(this);
 
 }
+
+var App = new AppCore();
